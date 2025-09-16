f09431118a076d78a1336b90359da0331385f1a5
Ihs4nn <ihsan.fikry@vodafone.com>
2025-09-16 15:23:24 +0100

    Adding code to update non-numerical values

--- credit_dataset/clean_data.py
+++ credit_dataset/clean_data.py
@@ -2,8 +2,15 @@ import pandas as pd
 
 dataset = pd.read_csv("raw_data/orginial_data.csv")
 
-# Replace all NA values with 'Test' for testing purposes
-dataset.fillna('Test', inplace=True)
-dataset.to_csv("modified_data.csv", index=False)
-print("Cleaned data saved to modified_data.csv")
+# Goes through the dataset and replaces 'NA' values with -1
+for col in dataset.columns:
+    dataset[col] = dataset[col].replace('NA', -1)
+
+# Update 'Risk' column, missing values are kept as -1
+if 'Risk' in dataset.columns:
+    dataset['Risk'] = dataset['Risk'].map({'good': 0, 'bad': 1, -1: -1})
+
+# Saves the cleaned dataset to a new CSV file for ML training
+dataset.to_csv("cleaned_data.csv", index=False)
+print("Cleaned data saved to cleaned_data.csv")
 
