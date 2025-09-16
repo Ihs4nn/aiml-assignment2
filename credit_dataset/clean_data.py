eabded4927b638856ef22d212a5f15d7616e4b55
Ihs4nn <ihsan.fikry@vodafone.com>
2025-09-16 15:01:25 +0100

    Test to check opening, updating and writing to a new csv file works

--- credit_dataset/clean_data.py
+++ credit_dataset/clean_data.py
@@ -1,3 +1,9 @@
 import pandas as pd
 
-dataset = pd.read_csv("/credit_dataset/raw_data/credit_data.csv")
+dataset = pd.read_csv("/credit_dataset/raw_data/original_data.csv")
+
+# Replace all NA values with 'Test' for testing purposes
+dataset.replace('NA', 'Unknown', inplace=True)
+dataset.to_csv("/credit_dataset/clean_data/cleaned_data.csv", index=False)
+print("Cleaned data saved to /credit_dataset/clean_data/cleaned_data.csv")
+
