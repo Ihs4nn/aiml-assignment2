4f81e772e16e2164b999c317aae4a697c4f22ce6
Ihs4nn <ihsan.fikry@vodafone.com>
2025-09-16 15:10:06 +0100

    Updated replace to fillna as pandas recognises Na as NaN

--- credit_dataset/clean_data.py
+++ credit_dataset/clean_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 dataset = pd.read_csv("raw_data/orginial_data.csv")
 
 # Replace all NA values with 'Test' for testing purposes
-dataset.replace('NA', 'Unknown', inplace=True)
+dataset.fillna('Test', inplace=True)
 dataset.to_csv("modified_data.csv", index=False)
 print("Cleaned data saved to modified_data.csv")
 
