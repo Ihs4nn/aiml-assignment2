8723e9554fadd104220869487c8c090f1638f0bd
Ihs4nn <ihsan.fikry@vodafone.com>
2025-09-24 16:45:56 +0100

    Adding random forest model to check which responds better

--- ml_component/train_and_evaluate.py
+++ ml_component/train_and_evaluate.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import LogisticRegression
-from sklearn.tree import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, average_precision_score
 
 # Load the cleaned dataset for training
-df = pd.read_csv("credit_dataset/cleaned_data.csv")
+df = pd.read_csv("../credit_dataset/cleaned_data.csv")
 feature_cols = ['Age', 'Sex', 'Job', 'Housing', 'Saving accounts',
                'Checking account', 'Credit amount', 'Duration', 'Purpose']
 X = df[feature_cols]
@@ -22,12 +22,24 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Training logistic regression model
+print("\n")
+# Training Logistic Regression model
 lr = LogisticRegression()
 lr.fit(X_train_scaled, y_train)
 y_pred_lr = lr.predict(X_test_scaled)
-# Printing logistic regression results
+# Printing Logistic Regression Model results
 print("Logistic Regression Results:")
 print(classification_report(y_test, y_pred_lr))
 print("ROC-AUC:", roc_auc_score(y_test, lr.predict_proba(X_test_scaled)[:, 1]))
-
+print("PR_AUC:", average_precision_score(y_test, lr.predict_proba(X_test_scaled)[:, 1]))
+print("\n")
+# Training Random Forest model
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+y_pred_rf = rf.predict(X_test)
+# Printing Random Forest Model results
+print("Random Forest Results:")
+print(classification_report(y_test, y_pred_rf))
+print("ROC-AUC:", roc_auc_score(y_test, rf.predict_proba(X_test)[:, 1]))
+print("PR_AUC:", average_precision_score(y_test, rf.predict_proba(X_test)[:, 1]))
+print("\n")
