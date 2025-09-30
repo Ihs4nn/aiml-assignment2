ced27e58d2d625a30be69773bc425e72a64756db
Ihs4nn <ihsan.fikry@vodafone.com>
2025-09-30 14:08:45 +0100

    Chosen Random Forest model to progress with

--- ml_component/train_and_evaluate.py
+++ ml_component/train_and_evaluate.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, average_precision_score
-from imblearn.over_sampling import SMOTE
+from sklearn.utils.class_weight import compute_class_weight
 
 # Load the cleaned dataset for training
 df = pd.read_csv("../credit_dataset/cleaned_data.csv")
@@ -20,30 +20,32 @@ y = df['Risk']
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, stratify=y)
 
-# Balance the classes in the training set
-smote = SMOTE(random_state=42)
-X_train_bal, y_train_bal = smote.fit_resample(X_train, y_train)
+# Compute class weights for imbalance
+y_classes = np.unique(y_train)
+class_weights = compute_class_weight(class_weight='balanced', classes=y_classes, y=y_train)
+cw_dict = {cls: weight for cls, weight in zip(y_classes, class_weights)}
 
 # Feature scaling
 scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train_bal)
+X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-print("\n")
-# Training Logistic Regression model
-lr = LogisticRegression()
-lr.fit(X_train_scaled, y_train_bal)
-y_pred_lr = lr.predict(X_test_scaled)
-# Printing Logistic Regression Model results
-print("Logistic Regression Results:")
-print(classification_report(y_test, y_pred_lr))
-print("ROC-AUC:", roc_auc_score(y_test, lr.predict_proba(X_test_scaled)[:, 1]))
-print("PR_AUC:", average_precision_score(y_test, lr.predict_proba(X_test_scaled)[:, 1]))
-print("\n")
+# Commented out rejected model for documentation purposes
+# print("\n")
+# # Training Logistic Regression model
+# lr = LogisticRegression(class_weight=cw_dict)
+# lr.fit(X_train_scaled, y_train)
+# y_pred_lr = lr.predict(X_test_scaled)
+# # Printing Logistic Regression Model results
+# print("Logistic Regression Results:")
+# print(classification_report(y_test, y_pred_lr))
+# print("ROC-AUC:", roc_auc_score(y_test, lr.predict_proba(X_test_scaled)[:, 1]))
+# print("PR_AUC:", average_precision_score(y_test, lr.predict_proba(X_test_scaled)[:, 1]))
+# print("\n")
 
 # Training Random Forest model
-rf = RandomForestClassifier()
-rf.fit(X_train_bal, y_train_bal)
+rf = RandomForestClassifier(class_weight=cw_dict)
+rf.fit(X_train, y_train)
 y_pred_rf = rf.predict(X_test)
 # Printing Random Forest Model results
 print("Random Forest Results:")
