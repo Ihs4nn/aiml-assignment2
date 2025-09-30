a1f93562678464f922fba46ec3b78c4aaba56d5f
Ihs4nn <ihsan.fikry@vodafone.com>
2025-09-30 13:28:04 +0100

    Added balancing to improve model results

--- ml_component/train_and_evaluate.py
+++ ml_component/train_and_evaluate.py
@@ -5,27 +5,34 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, average_precision_score
+from imblearn.over_sampling import SMOTE
 
 # Load the cleaned dataset for training
 df = pd.read_csv("../credit_dataset/cleaned_data.csv")
+
+# Define features and target
 feature_cols = ['Age', 'Sex', 'Job', 'Housing', 'Saving accounts',
-               'Checking account', 'Credit amount', 'Duration', 'Purpose']
+               'Checking account', 'Credit amount', 'Duration', 'Purpose', 'Credit score', 'Income']
 X = df[feature_cols]
 # Seperate target variable (what we want to predict)
 y = df['Risk']
 
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, stratify=y)
+
+# Balance the classes in the training set
+smote = SMOTE(random_state=42)
+X_train_bal, y_train_bal = smote.fit_resample(X_train, y_train)
 
 # Feature scaling
 scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
+X_train_scaled = scaler.fit_transform(X_train_bal)
 X_test_scaled = scaler.transform(X_test)
 
 print("\n")
 # Training Logistic Regression model
 lr = LogisticRegression()
-lr.fit(X_train_scaled, y_train)
+lr.fit(X_train_scaled, y_train_bal)
 y_pred_lr = lr.predict(X_test_scaled)
 # Printing Logistic Regression Model results
 print("Logistic Regression Results:")
@@ -33,9 +40,10 @@ print(classification_report(y_test, y_pred_lr))
 print("ROC-AUC:", roc_auc_score(y_test, lr.predict_proba(X_test_scaled)[:, 1]))
 print("PR_AUC:", average_precision_score(y_test, lr.predict_proba(X_test_scaled)[:, 1]))
 print("\n")
+
 # Training Random Forest model
 rf = RandomForestClassifier()
-rf.fit(X_train, y_train)
+rf.fit(X_train_bal, y_train_bal)
 y_pred_rf = rf.predict(X_test)
 # Printing Random Forest Model results
 print("Random Forest Results:")
