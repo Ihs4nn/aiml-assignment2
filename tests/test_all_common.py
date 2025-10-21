389d36425ef084e13f03b57e40b3cf9d5e25d77f
Ihs4nn <ihsan.fikry@vodafone.com>
2025-10-21 16:17:27 +0100

    Adding code for ALL02

--- tests/test_all_common.py
+++ tests/test_all_common.py
@@ -8,7 +8,7 @@ from ml_component.logical_regression import load_and_preprocess
 def test_successful_data_loading(valid_csv_path):
     with patch('pandas.read_csv', return_value=pd.read_csv(valid_csv_path)):
         X_train_scaled, X_test_scaled, y_train, y_test, cw_dict = load_and_preprocess()
-    
+        # Check that the returned values are as expected
         assert X_train_scaled.shape[0] > 0
         assert X_test_scaled.shape[0] > 0
         assert len(y_train) > 0
@@ -18,3 +18,10 @@ def test_successful_data_loading(valid_csv_path):
         assert all(isinstance(value, float) for value in cw_dict.values())
 
 # Test ID: ALL02
+def test_file_not_found():
+    with patch('pandas.read_csv', side_effect=FileNotFoundError):
+        # Checks that the function exits the program
+        with pytest.raises(SystemExit):
+            load_and_preprocess()
+
+# Test ID: ALL03
