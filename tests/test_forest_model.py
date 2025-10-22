905d223390b8b7c23611e8fa290c31fb1c3fba25
Ihs4nn <ihsan.fikry@vodafone.com>
2025-10-22 10:36:23 +0100

    Adding code for RF02, RF01 passed successfully

--- tests/test_forest_model.py
+++ tests/test_forest_model.py
@@ -8,3 +8,11 @@ def test_check_feature_output(trained_rf_model):
     assert rf_model.feature_importances_.size > 0
     # Check that the sum of importances is around 1
     assert np.isclose(rf_model.feature_importances_.sum(), 1.0)
+
+# Test RF02
+def test_rf_estimators_output(trained_rf_model):
+    rf_model, _ = trained_rf_model
+    # Check that the number of estimators is as expected and length is equal
+    assert hasattr(rf_model, 'n_estimators')
+    assert rf_model.n_estimators == 100
+    assert len(rf_model.estimators_) == 100
