a80f7a31140f992bf609b10aa3f94c4463a5ec46
Sharlene Cera <cera.sharlene@gmail.com>
2025-11-04 14:49:30 +0000

    Updated the GUI to have option menus for those that have a certain set of values to input.

--- ml_component/app.py
+++ ml_component/app.py
@@ -32,26 +32,84 @@ class LoanAppGUI(tk.Tk):
         tk.Label(self, text="Age*:").pack()
         self.age_entry = tk.Entry(self)
         self.age_entry.pack()
+
         # Sex field
+        genders = ["1", "2"] #TODO: Change these lists for better user readability
+        self.sex_entry = tk.StringVar(self)
+        self.sex_entry.set("Choose your gender")
+
         tk.Label(self, text="Sex* (1=Female, 2=Male):").pack()
-        self.sex_entry = tk.Entry(self)
-        self.sex_entry.pack()
+        self.gender_selection = tk.OptionMenu(self, self.sex_entry, *genders)
+        self.gender_selection.config(
+            bg="black",
+            fg="white",
+            relief="solid",
+            bd=1,
+            highlightthickness=0,
+            font=("Arial", 11),
+            width=16,
+        )
+        self.gender_selection.pack()
+
         # Job field
-        tk.Label(self, text="Jobs* (1=Employed, 2=Self-Employed, 3=Unemployed):").pack()
+        tk.Label(self, text="Jobs*:").pack()
         self.job_entry = tk.Entry(self)
         self.job_entry.pack()
+
         # Housing
+        housing_statuses = ["1", "2", "3"] #TODO: Change these lists for better user readability
+        self.housing_entry = tk.StringVar(self)
+        self.housing_entry.set("Choose your housing status")
+
         tk.Label(self, text="Housing* (1=Free, 2=Own, 3=Rent):").pack()
-        self.housing_entry = tk.Entry(self)
-        self.housing_entry.pack()
+        self.housing_selection = tk.OptionMenu(self, self.housing_entry, *housing_statuses)
+        self.housing_selection.config(
+            bg="black",
+            fg="white",
+            relief="solid",
+            bd=1,
+            highlightthickness=0,
+            font=("Arial", 11),
+            width=16,
+        )
+        self.housing_selection.pack()
+
         # Saving accounts
-        tk.Label(self, text="Saving Accounts* (1=Low, 2=Medium, 3=None, 4=Very High):").pack()
-        self.saving_entry = tk.Entry(self)
-        self.saving_entry.pack()
+        saving_accounts_statuses = ["1", "2", "3", "4"] #TODO: Change these lists for better user readability
+        self.saving_entry = tk.StringVar(self)
+        self.saving_entry.set("Choose your saving accounts status")
+
+        tk.Label(self, text="Saving Accounts* (1=Low, 2=Medium, 3=None, 4=High):").pack()
+        self.saving_selection = tk.OptionMenu(self, self.saving_entry, *saving_accounts_statuses)
+        self.saving_selection.config(
+            bg="black",
+            fg="white",
+            relief="solid",
+            bd=1,
+            highlightthickness=0,
+            font=("Arial", 11),
+            width=16,
+        )
+        self.saving_selection.pack()
+
         # Checking account
-        tk.Label(self, text="Checking Account* (1=Low, 2=Medium, 3=None, 4=Very High):").pack()
-        self.checking_entry = tk.Entry(self)
-        self.checking_entry.pack()
+        checking_account_statuses = ["1", "2", "3", "4"] #TODO: Change these lists for better user readability
+        self.checking_entry = tk.StringVar(self)
+        self.checking_entry.set("Choose your checking account status")
+
+        tk.Label(self, text="Checking Account* (1=Low, 2=Medium, 3=None, 4=High):").pack()
+        self.checking_selection = tk.OptionMenu(self, self.checking_entry, *checking_account_statuses)
+        self.checking_selection.config(
+            bg="black",
+            fg="white",
+            relief="solid",
+            bd=1,
+            highlightthickness=0,
+            font=("Arial", 11),
+            width=16,
+        )
+        self.checking_selection.pack()
+
         # Credit amount
         tk.Label(self, text="Credit Amount*:").pack()
         self.credit_entry = tk.Entry(self)
@@ -61,9 +119,23 @@ class LoanAppGUI(tk.Tk):
         self.duration_entry = tk.Entry(self)
         self.duration_entry.pack()
         # Purpose
-        tk.Label(self, text="Purpose* (0=Business, 1=Car, 2=Domestic Appliances, 3=Education, 4=Furniture/Equipment, 5=Radio/TV, 6=Repairs, 7=Vacation/Others):").pack()
-        self.purpose_entry = tk.Entry(self)
-        self.purpose_entry.pack()
+        purposes = ["1", "2", "3", "4", "5"] #TODO: Change these lists for better user readability
+        self.purpose_entry = tk.StringVar(self)
+        self.purpose_entry.set("Choose your purpose")
+
+        tk.Label(self, text="Purpose* (1=Business, 2=Car, 3=Education, 4=Furniture/Equipment, 5=Radio/TV").pack()
+        self.purpose_selection = tk.OptionMenu(self, self.purpose_entry, *purposes)
+        self.purpose_selection.config(
+            bg="black",
+            fg="white",
+            relief="solid",
+            bd=1,
+            highlightthickness=0,
+            font=("Arial", 11),
+            width=16,
+        )
+        self.purpose_selection.pack()
+
         # Credit Score
         tk.Label(self, text="Credit Score*:").pack()
         self.credit_score_entry = tk.Entry(self)
